2732e825caf214700ddc61dd0355c75bc4f7b233
Léana 江 <leana.jiang+git@icloud.com>
2024-09-21 01:43:22 +0200

    painting now works

--- view.js
+++ view.js
@@ -1,25 +1,31 @@
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
-//
 
+function setStyle(obj, ctx) {
+	ctx.strokeStyle = obj.color;
+	ctx.lineWidth = obj.thickness;
+}
 
 Rectangle.prototype.paint = function(ctx) {
-	ctx.strokeStyle(this.color)
+	setStyle(this, ctx);
+	ctx.stroke;
 	ctx.beginPath();
-	ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(), this.getFinalY());
+	ctx.rect(this.originX, this.originY, this.originX + this.width, this.originY + this.height);
 	ctx.stroke();
 };
 
 Line.prototype.paint = function(ctx) {
-	ctx.strokeStyle(this.color)
+	setStyle(this, ctx);
 	ctx.beginPath();
-	ctx.moveTo(this.getInitX(), this.getInitY());
-	ctx.lineTo(this.getFinalX(), this.getFinalY());
+	ctx.moveTo(this.startX, this.startY);
+	ctx.lineTo(this.endX, this.endY);
 	ctx.stroke();
 };
 
 Drawing.prototype.paint = function(ctx) {
 	ctx.fillStyle = '#F0F0F0';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	this.getForms().forEach(form => form.paint(ctx));
+	this.getForms().forEach(form => {
+		form.paint(ctx);
+	});
 };
 
