14c317dc77749898b15e3db7e6495732e4fba78e
Léana 江 <leana.jiang+git@icloud.com>
2024-09-20 23:16:14 +0200

    Q6

--- model.js
+++ model.js
@@ -1,3 +1,29 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Line(color, thickness, startX, startY, endX, endY) {
+	Shape.call(color, thickness);
+	this.startX = startX;
+	this.startY = startY;
+	this.endX = endX;
+	this.endY = endY;
+}
+Line.prototype = new Shape();
+
+function Rectangle(originX, originY, width, height) {
+	Shape.call(color, thickness);
+	originX = 0;
+	originY = 0;
+	width = 0;
+	height = 0;
+}
+Rectangle.prototype = new Shape();
+
+function Shape(color, thickness) {
+	this.color = color;
+	this.thickness = thickness;
+}
+
+function Drawing() {
+	this.drawings = [];
+}
