4a08ed2049511208231f5fc39a3c54da6f0e419a
Léana 江 <leana.jiang+git@icloud.com>
2024-09-21 01:42:41 +0200

    reworked model

--- model.js
+++ model.js
@@ -1,33 +1,37 @@
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
 
-function Line(color, thickness, startX, startY, endX, endY) {
-	Shape.call(color, thickness);
-	this.startX = startX;
-	this.startY = startY;
-	this.endX = endX;
-	this.endY = endY;
-}
-Line.prototype = new Shape();
-
-function Rectangle(originX, originY, width, height) {
-	Shape.call(color, thickness);
-	originX = 0;
-	originY = 0;
-	width = 0;
-	height = 0;
-}
-Rectangle.prototype = new Shape();
-
 function Shape(color, thickness) {
 	this.color = color;
 	this.thickness = thickness;
 }
 
+function Line(color, thickness, startX, startY, endX, endY) {
+	Shape.call(this, color, thickness);
+	this.startX = startX;
+	this.startY = startY;
+	this.endX = endX;
+	this.endY = endY;
+}
+Line.prototype = Shape;
+
+function Rectangle(color, thickness, originX, originY, width, height) {
+	Shape.call(this, color, thickness);
+	this.originX = originX;
+	this.originY = originY;
+	this.width = width;
+	this.height = height;
+}
+Rectangle.prototype = new Shape();
+
 function Drawing() {
 	this.forms = [];
 
 	this.getForms = () => {
-		return forms;
+		return this.forms;
+	}
+
+	this.addForm = form => {
+		return this.forms.push(form)
 	}
 }
